07d5c0b381442972ff65293affba2f49152436ab
Aarti Kumari <aartikumari31102003@gmail.com>
2023-10-14 11:36:03 +0530

    using html and css

---
+++ hostar.js
@@ -0,0 +1,15 @@
+let cardContainers=[...document.querySelectorAll('.card-container')];
+let preBtns=[...document.querySelectorAll('.pre-btn')];
+let nxtBtns=[...document.querySelectorAll('.nxt-btn')];
+cardContainers.forEach((item, i)=>{
+    let containerDimensions=item.getBoundingClientRect();
+    let containerWidth=containerDimensions.width;
+
+    nxtBtns[i].addEventListener('click', ()=>{
+        item.scrollleft+=containerWidth-200;
+    })
+
+    preBtns[i].addEventListener('click', ()=>{
+        item.scrollleft-=containerWidth+200;
+    })
+})
